0074f672fdd8285128e8721d31472b91cb024a9e
John Garner <jdextergarneriv@gmail.com>
2020-10-02 18:45:56 -0400

    adding functions to save textarea input on click to local storage.

--- script.js
+++ script.js
@@ -2,26 +2,21 @@ var jumbotronEl = document.getElementsByClassName('jumbotron');
 var currentDayEl = document.getElementById('currentDay');
 var containerEl = document.getElementsByClassName('container');
 var buttonEl = document.querySelectorAll('button');
-
+var textAreaEl = document.querySelectorAll('textarea');
+// Variable for current date to be displayed at top of page
 var m = moment().format('MMMM Do YYYY, h:mm:ss a');
 var theCurrentDate = m.toString();
-console.log(theCurrentDate);
-
+// Variable for hour format to be compared against hoursOfDay
 var currentHour = moment().format("H")
 var currentHourString = currentHour.toString();
 console.log('current hour', currentHourString);
-
-var textAreaEl = document.querySelectorAll('textarea');
-console.log('these are text areas', textAreaEl);
-
-
-
 
 // Function that displays current date in jumbotron
 function displayCurrentDate() {
     $('#currentDay').append(theCurrentDate);
 }
 
+//Hours of day variable to be looped and attribute of 'hour' assigned to to textarea
 var hoursOfDay = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
 
 function renderCalendar() {
@@ -29,31 +24,56 @@ function renderCalendar() {
     for (i = 0; i < hoursOfDay.length; i++){
     $('textarea').attr("hour", hoursOfDay[i]);
     var assignedHour = $('textarea').attr("hour");
-
+        
     if (currentHourString > assignedHour) {
         console.log('in the past', assignedHour)
-        
         $('textarea').addClass('past');
-       
+        
     } 
     else if (currentHourString === assignedHour) {
         console.log('in the present', assignedHour);
+        isPastLoaded = true;
         $('textarea').addClass('present');
         
     } 
     else {
         console.log('In the future', assignedHour);
         $('textarea').addClass('future');
-    }
-
-    };
+    }};
 };
-    
+// Work on function to render events after storage.
+function showCalendarEvents(){
+var allEvents = localStorage.getItem('storedEvents');
+console.log('these are the stored events', allEvents);
+};
+// Function to save stored events.
+function saveCalendarEvents() {
+   localStorage.setItem('storedEvents', JSON.stringify(storedEvents));
+};
+// Variable to push events into
+var storedEvents = [];
+// On click save button should push text area string into storedEvents
+function submitEvents(){
+
+    $('button').on("click", function(event){
+    event.preventDefault();
+    var addEvent = $.trim($(textAreaEl).val());
+    console.log('the event', addEvent);
+
+    if (addEvent === ""){
+        return;
+    }
+    storedEvents.push(addEvent);
+
+    saveCalendarEvents();
+    });
+};
 
 
 function startCalendar() {
 displayCurrentDate ();
 renderCalendar();
+submitEvents();
 
 };
 
